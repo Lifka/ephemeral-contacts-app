06a3784fa382730828550bd3b0132ae8bc73c21f
Lifka <javier@izquierdovera.com>
2016-12-30 18:37:38 +0100

    Initial commit

---
+++ app/src/main/java/com/javierizquierdovera/miguelmedina/ephemeralcontacts/ephemeralcontacts/contactlist.java
@@ -0,0 +1,13 @@
+package com.javierizquierdovera.miguelmedina.ephemeralcontacts.ephemeralcontacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class contactlist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contactlist);
+    }
+}
